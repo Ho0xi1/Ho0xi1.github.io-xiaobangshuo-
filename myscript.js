fa2d08d220879c07bb69917f112b381042a19a4d
Ho0xi1 <115571903+Ho0xi1@users.noreply.github.com>
2022-12-03 19:25:45 +0000

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,11 @@
+ function yourFunction(){
+    document.getElementById("dick").innerHTML="都当抽象小鬼了，没点抗压能力怎么行呢？^^";
+ }
+
+
+ 
+document.getElementById("myalart").addEventListener("click", hisfunction);
+
+function hisfunction(){
+    alert("new new zha le");
+}
